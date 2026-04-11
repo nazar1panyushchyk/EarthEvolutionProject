03e3acc191ba3de900ad973dc3c79b56d69d863a
nazar1panyushchyk <npanusik@gmail.com>
2026-04-11 20:21:55 +0300

    added approximate interface view

---
+++ EarthEvolutionProject/Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace EarthEvolutionProject
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
