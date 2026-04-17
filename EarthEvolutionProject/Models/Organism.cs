93b812f7123b3c8adc4a9b3d1d09cf0a1f11e959
nazar1panyushchyk <npanusik@gmail.com>
2026-04-17 14:34:30 +0300

    added routing between periods & data transfer to project

---
+++ EarthEvolutionProject/Models/Organism.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+public class Organism
+{
+    public required string Id { get; set; }
+    public required string CommonName { get; set; }
+    public required string ScientificName { get; set; }
+    public required string Type { get; set; }
+    public required string Image { get; set; }
+}
---
+++ EarthEvolutionProject/Models/Period.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+public class Period
+{
+    public required string Id { get; set; }
+    public required string Era { get; set; }
+    public required string Name { get; set; }
+    public required string Timeframe { get; set; }
+    public required string MainImage { get; set; }
+    public required string Description { get; set; }
+    public List<Organism> Organisms { get; set; } = [];
+}
